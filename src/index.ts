6933734161ce6a1d25deb0b188e511c41d7bbf01
Mabel Samuel <mapleberrysamuel@gmail.com>
2026-01-09 00:14:40 +0100

    implemented cors

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import express from 'express';
 import type { Request, Response } from 'express';
 import dotenv from "dotenv";
 import pool from '../src/db';
+import cors from "cors";
+import type { CorsOptions } from 'cors';
 import authRoutes from './routes/auth.route';
 import userRoutes from "./routes/user.route";
 import bookRoutes from "./routes/book.route";
@@ -11,6 +13,28 @@ dotenv.config();
 
 const app = express()
 const port = process.env.PORT
+
+const allowedOrigins = [process.env.FRONTEND_URL, process.env.DEV_URL].filter(Boolean) as string[];
+
+const corsOptions: CorsOptions = {
+  origin: (
+    origin: string | undefined,
+    callback: (err: Error | null, allow?: boolean) => void
+  ) => {
+    if (process.env.NODE_ENV === "development") {
+      callback(null, true);
+    } 
+    else if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+};
+
+app.use(cors(corsOptions));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
